61528f5cde23f83466a4b311ef0785213ad81268
Ryan Szalanski <44514729+rszalanski@users.noreply.github.com>
2019-08-07 19:52:28 -0400

    Added list frame
    Added a frame which expands/contracts when the window size is changed and will contain the list of scheduled launches

--- main.py
+++ main.py
@@ -22,13 +22,18 @@ def refreshList():
 		
 		
 	# Create main window
-	master = tk.Tk(className="Upcoming Space Launch List")
+	master = tk.Tk(className="\\Upcoming Space Launch List")
 	master.configure(bg=darkblue)
 	# Create title box
-	titleBox = tk.Frame(master, bg=darkblue)
-	titleBox.pack(side=TOP, fill=BOTH)
+	titleBox = tk.Frame(master, bg=darkblue, relief=RAISED, borderwidth=2, height=5)
+	titleBox.pack(side=TOP, fill=BOTH, expand=False)
 	titleTxt = tk.Label(titleBox, text='Upcoming Space Launches', foreground='white', bg=darkblue)
 	titleTxt.pack()
+	
+	
+	# Create box containing upcoming launch info
+	launchBox = tk.Frame(master, bg='cyan', relief=SUNKEN, borderwidth=3, width=500, height=500)
+	launchBox.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
 
 
