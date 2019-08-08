7ccf95e9fcfdf2e610a6ab352a856df106798cf9
Ryan Szalanski <44514729+rszalanski@users.noreply.github.com>
2019-08-07 20:09:39 -0400

    Added 'EXIT' and 'REFRESH' buttons
    Added a red 'EXIT' button and green 'REFRESH' at the bottom of the window

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import requests
 
 
 
+# Function to call to exit the program
+def exitProgram():
+	global master
+	master.destroy()
+	pass
+
+
+
 # Function to refresh the window by closing and re-opening it
 def refreshList():
 	# Define window colors
@@ -34,6 +42,18 @@ def refreshList():
 	# Create box containing upcoming launch info
 	launchBox = tk.Frame(master, bg='cyan', relief=SUNKEN, borderwidth=3, width=500, height=500)
 	launchBox.pack(fill=BOTH, expand=True, padx=10, pady=10)
+	
+	
+	# Create list of upcoming launches
+	
+	
+	# Create 'EXIT' and 'REFRESH' buttons
+	buttonBox = tk.Frame(master, bg=darkblue, relief=RAISED, borderwidth=2, height=50)
+	buttonBox.pack(side=BOTTOM, fill=BOTH, expand=False)
+	exitButton = tk.Button(buttonBox, bg='red', foreground='white', text='EXIT', command=exitProgram)
+	exitButton.pack(side=LEFT, fill=BOTH, expand=True)
+	refrButton = tk.Button(buttonBox, bg='green', foreground='white', text='REFRESH', command=refreshList)
+	refrButton.pack(side=RIGHT, fill=BOTH, expand=True)
 
 
 
